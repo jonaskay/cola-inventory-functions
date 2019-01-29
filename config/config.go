9201ffc2ef24c6c1e9d74b4734d876688f9bf557
Jonas Kay <jonaskay@iki.fi>
2019-01-29 19:04:34 +0200

    Record the delivery time

--- config/config.go
+++ config/config.go
@@ -4,7 +4,7 @@ import "time"
 
 // Order is a struct for Order datastore entities.
 type Order struct {
-	ID        int64     `jsonapi:"primary,order" datastore:"-"`
-	CreatedAt time.Time `jsonapi:"attr,created_at"`
-	Delivered bool      `jsonapi:"attr,delivered"`
+	ID          int64     `jsonapi:"primary,order" datastore:"-"`
+	CreatedAt   time.Time `jsonapi:"attr,created_at"`
+	DeliveredAt time.Time `jsonapi:"attr,delivered_at"`
 }
